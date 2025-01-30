b99dbcc77c5d182d049a4d084d2525abe30a6116
Trevor Lawson <trev3law@byu.edu>
2025-01-30 15:19:22 -0700

    routing fixed

---
+++ app/about.web.tsx
@@ -0,0 +1,30 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is the test web about page</Text>
+      <Link href="./crm" style={styles.button}>
+        Go to the crm page
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "#ffffff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, Text, YStack, TamaguiProvider } from "tamagui";
+import config from "../tamagui.config";
+
+export default function Index() {
+  return (
+    <TamaguiProvider config={config}>
+      <YStack>
+        <Text>Welcome to Tamagui! How are you?</Text>
+        <Button onPress={() => console.log("Button pressed")}>Click Me</Button>
+      </YStack>
+    </TamaguiProvider>
+  );
+}
