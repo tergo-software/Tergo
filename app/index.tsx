a372f923f4c849379d0cd4cd48056133b7eaf978
sjack5 <sjacklin01@gmail.com>
2025-02-01 10:00:13 -0700

    Added a basic page for index, apartments, and inspection. Reconfigured the tamagui.config file as well so we have a nice looking layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,30 @@
-import React from "react";
-import { Button, Text, YStack, TamaguiProvider } from "tamagui";
+import React, { useState } from "react";
+import { Button, Text, YStack, Stack } from "tamagui";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { Link } from "expo-router";
+import { Linking, Pressable } from 'react-native';
+import Apartments from "./apartments";
+import { TamaguiProvider } from "tamagui";
 import config from "../tamagui.config";
 
 export default function Index() {
+  const navigation = useNavigation();
+
   return (
-    <TamaguiProvider config={config}>
-      <YStack>
-        <Text>Welcome to Tamagui! How are you?</Text>
-        <Button onPress={() => console.log("Button pressed")}>Click Me</Button>
-      </YStack>
+    <TamaguiProvider config={config} defaultTheme="light">
+      <Stack flex={1} backgroundColor="$background" alignItems="center" justifyContent="center" padding="$4">
+        <YStack space="$4" alignItems="center">
+          <Text fontSize="$6" fontWeight="bold" color="$color">
+            Welcome to Tergo!
+          </Text>
+          <Link href={"/apartments"} style={{ marginHorizontal: 'auto'}} asChild>
+            <Pressable>
+              <Text>Go to apartments</Text>
+            </Pressable>
+          </Link>
+        </YStack>
+      </Stack>
     </TamaguiProvider>
   );
 }
